c6ffb34943105383bcbb8da073fe2405211d49a8
Johán Steven Arias <jsarias1993@gmail.com>
2018-08-06 22:31:06 -0500

    Primer Commit

---
+++ example/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+
+class SignUpView(CreateView):
+	template_name = 'example/signup.html'
+	form_class = UserCreationForm
+
+def validate_username(request):
+	username = request.GET.get('username', None)
+
+	data = {
+		'is_taken': User.objects.filter(username__iexact=username).exists()
+	}
+
+	if data['is_taken']:
+		data['status'] = 'A user with this username already exists'
+	else:
+		data['status'] = 'The Username is available'
+
+	return JsonResponse(data)
+
+
